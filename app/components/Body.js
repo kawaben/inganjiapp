b31c8e119ea2736bb8a006fef53f3831b0d35bb1
kawaben <kabagemabenj@gmail.com>
2025-03-27 15:00:05 +0200

    just updated product list location

--- app/components/Body.js
+++ app/components/Body.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import "../globals.css";
-import Products from "../api/products/page";
+import Products from "../products/page";
 
 export default function Home() {
     return (
--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -1,4 +1,6 @@
-import { useRouter } from "next/router";
+'use client'
+
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 
 const products = [
